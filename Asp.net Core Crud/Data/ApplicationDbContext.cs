ece15dd779b6553f2ee3885e1016da7a9417e185
Mohammed Fananah <Hp@HP>
2023-02-21 10:30:55 +0200

    Add project files.

---
+++ Asp.net Core Crud/Data/ApplicationDbContext.cs
@@ -0,0 +1,19 @@
+﻿using Asp.net_Core_Crud.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Asp.net_Core_Crud.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Employee> Employees { get; set; }
+    }
+}
---
+++ Asp.net Core Crud/Models/Employee.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Asp.net_Core_Crud.Models
+{
+    public class Employee
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string Position { get; set; }
+
+        public string Office { get; set; }
+
+        public int Age { get; set; }
+
+        public int Salary { get; set; }
+
+    }
+}
